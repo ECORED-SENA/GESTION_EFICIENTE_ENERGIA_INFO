b711db17561410f7b0214ec3ea60191b0cbd3d74
Luis Urueta <lgurueta@misena.edu.co>
2021-07-26 18:14:45 -0500

    Avance1

---
+++ src/config/global.js
@@ -0,0 +1,82 @@
+export default {
+  creditos: {
+    liderEquipo: [
+      {
+        nombre: 'Maria Camila Garcia Santamaria',
+        cargo: 'Líder del equipo',
+        centro: 'Dirección General',
+      },
+    ],
+    contenidoInstruccional: [
+      {
+        nombre: 'Rafael Neftalí Lizcano Reyes',
+        cargo: 'Asesor metodológico y pedagógico',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura Regional Santander',
+      },
+    ],
+    desarrolloProducto: [
+      {
+        nombre: 'Francisco José Lizcano Reyes',
+        cargo: 'Responsable del equipo',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura Regional Santander',
+      },
+      {
+        nombre: 'Leyson Fabian Castaño Perez',
+        cargo: 'Soporte organizacional',
+        centro: 'Centro de Comercio y Servicios Regional Tolima',
+      },
+      {
+        nombre: 'Oscar Ivan Uribe Ortiz',
+        cargo: 'Diseño web',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura Regional Santander',
+      },
+      {
+        nombre: 'Camilo Villamizar Lizcano',
+        cargo: 'Diseño web',
+        centro: 'Producción audiovisual',
+      },
+      {
+        nombre: 'Gilberto Junior Rodríguez Rodríguez',
+        cargo: 'Diseño web',
+        centro: 'Producción audiovisual',
+      },
+      {
+        nombre: 'Víctor Raúl Cárdenas Cáceres',
+        cargo: 'Diseño web',
+        centro: 'Producción audiovisual',
+      },
+      {
+        nombre: 'Wilson Andrés Arenales Cáceres',
+        cargo: 'Diseño web',
+        centro: 'Producción audiovisual',
+      },
+      {
+        nombre: 'Zuleidy Maria Ruiz Torres',
+        cargo: 'Diseño web',
+        centro: 'Producción audiovisual',
+      },
+      {
+        nombre: 'Luis Gabriel Urueta Alvarez',
+        cargo: 'Desarrollo Front-End',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura Regional Santander',
+      },
+      {
+        nombre: 'Nombre',
+        cargo: 'Validación de diseño y contenido',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura Regional Santander',
+      },
+    ],
+    gestoresRepositorio: [
+      {
+        nombre: 'Milady Tatiana Villamil Castellanos',
+        cargo: 'Validación y vinculación en plataforma LMS',
+        centro: 'Centro de Comercio y Servicios Regional Tolima',
+      },
+    ],
+  },
+}
---
+++ src/views/Inicio.vue
@@ -0,0 +1,270 @@
+<template lang="pug">
+section
+  .banner-principal.mb-5
+    .container.tarjeta.p-4.p-sm-5(
+      :style="{'background-image': `url(${require('@/assets/curso/fondo-banner-principal.png')})`}"
+    )
+      .row.justify-content-around.align-items-center
+        .col-lg-7.col-xxl-6.banner-principal__info
+          .banner-principal__programa
+            .h1.mb-0 Tecnología en Gestión Eficiente de la Energía
+          
+          .h2 Información del programa
+          .banner-principal__datos
+            ul
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
+                p.banner-principal__datos__item__texto Tecnología en Gestión Eficiente de la Energía
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
+                p.banner-principal__datos__item__texto 821207
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total: 
+                p.banner-principal__datos__item__texto 821207 Horas
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración: 
+                p.banner-principal__datos__item__texto 27 meses
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Modalidad: 
+                p.banner-principal__datos__item__texto virtual
+
+        .d-none.d-lg-block.col-lg-5
+          //- img(src="@/assets/curso/banner-princiapal.svg")
+          .video
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/4sUmWppw-u4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+  
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 1. Presentación
+
+    p  
+      | El 
+      strong Servicio Nacional de Aprendizaje – SENA
+      | , le da la bienvenida al programa de 
+      strong formación Tecnólogo en Gestión Eficiente de la Energía 
+      | en modalidad virtual. El objetivo principal del programa es formar talento humano capacitado en implementar 
+      strong sistemas de gestión eficiente de la energía 
+      | que permita a las empresas y organizaciones 
+      strong orientar acciones para mejorar su competitividad e impacto ambiental, 
+      | mediante el 
+      strong uso racional y eficiente de la energía, 
+      | la 
+      strong utilización de fuentes de energía renovable no convencionales 
+      | y 
+      strong la reducción de emisiones de gases de efecto invernadero.
+
+    p
+      | El egresado de este programa estará en capacidad de utilizar las diferentes herramientas de los sistemas de gestión de la energía para formular e 
+      strong implementar acciones de mejora e innovación energética 
+      | aplicados a los 
+      strong diferentes sectores económicos
+      | , integrando tecnologías de alta eficiencia que permitan disminuir el consumo energético y aprovechar las fuentes de energía no convencionales. Adicionalmente, desarrollará 
+      strong para la toma de decisiones. habilidades para la recolección, procesamiento y análisis de datos de diversas fuentes.
+
+    p 
+      | El programa está dirigido a 
+      strong ciudadanos colombianos 
+      | sin importar su género o formación previa, cumpliendo los siguientes requisitos mínimos: 15 años de edad, graduado de media académica (
+      strong grado undécimo
+      | ), haber presentado las pruebas saber y que cuente con disponibilidad de equipo de cómputo con acceso a internet.
+
+    p
+      | El programa de formación tecnológico comprende una etapa 
+      strong lectiva
+      | , en la cual el aprendiz desarrolla procesos de aprendizaje y otra etapa 
+      strong productiva 
+      | en la que se tiene la oportunidad de realizar prácticas en diferentes alternativas como contrato de aprendizaje, pasantías, proyecto productivo o unidades productivas familiares.
+
+    p 
+      | La modalidad de 
+      strong formación virtual 
+      | le brindará flexibilidad para ajustar sus tiempos de acuerdo con sus necesidades, a través de la plataforma de gestión de aprendizaje virtual, contando con el acompañamiento continuo del instructor a través de encuentros sincrónicos y asincrónicos.
+
+    h5.mb-0 ¡Bienvenido al SENA y te invitamos a hacer de Colombia un país productivo y sostenible!
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 2.	Justificación del programa
+
+    p En el entorno global, las acciones en eficiencia energética y su evolución hacia los sistemas de gestión de la energía constituyen un eje fundamental y decisivo en la reducción de emisiones de Gases de Efecto Invernadero (GEI) y la mitigación de los efectos del Cambio Climático. En tal sentido, su implementación se encuentra alineada con los Objetivos de Desarrollo Sostenible (ODS), y de manera principal con los de “Energía asequible y no contaminante”, “Ciudades y comunidades sostenibles” y “Producción y consumo responsables”, requiriendo personas con formación pertinente para su efectiva realización.  
+    p A nivel nacional, las disposiciones de las leyes 697 de 2001 y 1715 de 2014, establecen como un asunto de interés social, público y de conveniencia nacional el Uso Racional y Eficiente de la Energía (URE), así como también la integración de las energías renovables no convencionales y el desarrollo y promoción de la gestión eficiente de la energía, siendo implementadas bajo el Programa de Uso Racional y eficiente de la energía - PROURE, con los planes adoptados al efecto, el cual establece a “La educación, innovación y desarrollo tecnológico” como una de las estrategias y acciones base para para el cumplimiento de las metas sectoriales. En el Plan Energético Nacional (PEN) 2020 – 2050 se considera como uno de sus propósitos “(…) definir un modelo energético sostenible al 2050, que impulse la transformación energética para lograr el progreso económico, el mejoramiento de la calidad de vida y el respeto al medio ambiente.”. 
+    p Colombia, para la implementación de los ODS considera en su agenda 2030, entre otras metas, las de duplicar la tasa de mejora de la eficiencia energética; aumentar la cooperación internacional para facilitar el acceso a la investigación y tecnologías sobre la eficiencia energética, avanzadas y menos contaminantes; así como ampliar la infraestructura y mejorar la tecnología para prestar servicios energéticos modernos y sostenibles. En el mismo sentido, se encuentran las metas de reducción del impacto ambiental negativo de las ciudades; el apoyo mediante asistencia financiera y técnica para la construcción de edificios sostenibles y resilientes, así como la ampliación de actores y empresas que adoptan e implementan políticas y planes integrados para lograr la gestión sostenible y el uso eficiente de los recursos naturales con la adopción de prácticas sostenibles sobre reducción de residuos e incorporación de información sobre la sostenibilidad de sus ciclos. 
+    p El programa “Tecnología en Gestión Eficiente de la Energía”, dirigido a personas con conciencia ambiental y habilidades digitales, facilitará la implementación de metas dispuestas para el logro de los ODS. El campo de acción del egresado del programa se extiende a organizaciones públicas y privadas de diversos sectores económicos, tales como transporte, industrial, terciario, residencial, minero y agrícola, donde la gestión del consumo energético de diferentes fuentes, tales como gas, carbón, gasolina y energías renovables no convencionales, es visto de manera integrada a los ciclos productivos, facilitando la realización de diagnósticos, la elaboración y realización de planes de acción que redundan en mejoramiento de la productividad y la sostenibilidad de las organizaciones. Así mismo, la formación integral que brinda el programa fomenta la inclusión social, capacidades creativas y emprendedoras en el egresado.  
+    p La modalidad virtual del programa, lo hace asequible a personas que pueden estar localizadas en zonas apartadas, con lo cual a la vez que garantiza la disponibilidad, potencializa la reducción de las brechas de género y con ello se da cabida al acceso igualitario a la educación y a las carreras STEM (Science, Technology Engineering and Mathematics), las cuales apuntalan el Desarrollo Sostenible mediante la creatividad y la innovación. 
+    p El SENA con este programa de formación en el marco del “Pacto por los Empleos Verdes y Transición Justa en Colombia”, contribuye a la promoción de empleos que atienden las demandas de nuevas capacidades laborales para la adaptación del sector productivo, a los retos de productividad laboral y del crecimiento verde. 
+    p.mb-0 La oferta de programas en temáticas de gestión de la energía en el país se encuentra principalmente a nivel universitario, sugiriendo su ampliación bien en atención a los procesos de electrificación de sectores y actividades económicas, como resultado de estudios de brechas, tales como el publicado por el Clúster de Energía Eléctrica de la Cámara de Comercio de Bogotá (2018) denominado Identificación y cierre de brechas de capital humano, en términos de “Actividades y conocimientos con mayor demanda en los próximos diez años”,  identificando a la “Gestión y uso eficiente de la energía como sello estratégico de mercado”  y “Tendencia Organizacional Específica (TOE)”, en el mismo sentido hace referencia a la adaptación de tecnologías para el bajo consumo energético y la implementación de sistemas de generación de energía con fuentes no convencionales, entre otras, como una de las “Tendencias Tecnológicas Específicas (TTE)”. 
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 3. Competencias a desarrollar
+
+    p Competencias técnicas o específicas:
+
+    ul.lista-ul.mb-5
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Montar componentes eléctricos de acuerdo con procedimiento técnico
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Evaluar instalaciones eléctricas acorde con normativa y procedimientos técnicos.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Estructurar sistema de energías renovables según procedimiento técnico y normativa ambiental
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Implementar requisitos normativos de acuerdo con parámetros técnicos.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Configurar el sistema de gestión de la energía de acuerdo con la normativa y estándares técnicos.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Procesar datos de acuerdo con procedimiento técnico y metodología estadística.
+    
+    p Competencias claves:
+
+    ul.lista-ul.mb-5
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Aplicación de conocimientos de las ciencias naturales de acuerdo con situaciones del contexto productivo y social.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Utilizar herramientas informáticas de acuerdo con necesidades de manejo de información.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Interactuar en lengua inglesa de forma oral y escrita dentro de contextos sociales y laborales según los criterios establecidos por el marco común europeo de referencia para las lenguas.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Desarrollar procesos de comunicación eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal y productivo.    
+
+    p Competencias transversales: 
+
+    ul.lista-ul.mb-0
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Interactuar en el contexto productivo y social de acuerdo con principios éticos para la construcción de una cultura de paz.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo de acuerdo con las políticas organizacionales y la normatividad vigente.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Generar hábitos saludables de vida mediante la aplicación de programas de actividad física en los contextos productivos y sociales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Gestionar procesos propios de la cultura emprendedora y empresarial de acuerdo con el perfil personal y los requerimientos productivos y sociales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Orientar investigación formativa según referentes técnicos.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Ejercer derechos fundamentales del trabajo en el marco de la constitución política y los convenios internacionales.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 4. Perfil de ingreso
+
+    p.mb-0 El programa está dirigido a ciudadanos colombianos sin importar su género o formación previa, cumpliendo los siguientes requisitos mínimos: 15 años de edad, graduado de media académica (grado undécimo), haber presentado las pruebas saber y que cuente con disponibilidad de equipo de cómputo con acceso a internet.  También podrán acceder técnicos, tecnólogos con carreras afines a la Gestión Eficiente de la Energía.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 5. Perfil de egreso  
+
+    p El egresado del programa de Tecnología en Gestión de la Energía es un talento humano formado integralmente bajo el enfoque por competencias. Sus principales funciones son la elaboración de planes de acción de eficiencia energética, recopilación, procesamiento e interpretación de los datos de consumos energéticos, creación de líneas de base energéticas e Indicadores de Desempeño Energético. Igualmente, llevar a cabo la implementación y seguimiento de Sistemas de Gestión de la Energía encaminados a lograr la ejecución de soluciones para la disminución de consumos de energía, reducción de costos operativos y emisiones de Gases de Efecto Invernadero, que incluyen evaluación de alternativas para la generación de energía con fuentes renovables.
+    p El egresado estará en capacidad de realizar su labor en todo tipo de organizaciones públicas y privadas de diversos sectores económicos, y mediante su dinamismo, creatividad, capacidad crítica y emprendedora, podrá crear su propia empresa de servicios técnicos en el área de gestión eficiente de la energía.
+  
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 6. Estrategia metodológica  
+
+    p Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias. 
+
+    p Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el que hacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    .row.g-4.justify-content-center.mb-0
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/01.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El instructor - tutor
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/02.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El entorno
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/03.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong Las TIC
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/04.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El trabajo colaborativo
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 Créditos
+
+    
+    .creditos
+      div(
+        v-for="(creditoKey, index) of Object.keys(creditosData)"
+        :class="index != Object.keys(creditosData).length -1 ? 'mb-4' : ''" 
+      )
+        .creditos__titulo {{configTitulos[creditoKey]}}
+        table
+          tbody
+            tr(
+              v-for="(item, idx) of creditosData[creditoKey]" 
+              :key="creditoKey+idx"
+            )
+              td.text-bold(colspan='2' v-html="renderText(item.nombre)")
+              td(colspan='2' v-html="renderText(item.cargo)")
+              td(colspan='3' v-html="renderText(item.centro)")
+
+  Footer
+
+</template>
+<script>
+export default {
+  name: 'Inicio',
+  data: () => ({
+    configTitulos: {
+      liderEquipo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
+      contenidoInstruccional: 'CONTENIDO INSTRUCCIONAL',
+      desarrolloProducto:
+        'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
+      gestoresRepositorio: 'GESTORES DE REPOSITORIO',
+    },
+  }),
+  computed: {
+    creditosData() {
+      return this.$config.creditos
+    },
+  },
+  methods: {
+    renderText(textObj) {
+      let newText = ''
+      if (Array.isArray(textObj)) {
+        textObj.forEach((texto, index) => {
+          newText += (index ? '<br/>' : '') + texto
+        })
+      } else {
+        newText += textObj
+      }
+      return newText
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.creditos
+  td, th
+    border-color: $color-sistema-c
+</style>
